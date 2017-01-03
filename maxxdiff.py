a9b0d976c9f044ded1447ea9fca69f5027ebba83
subbu0109 <subbu0109@gmail.com>
2017-01-03 13:00:07 -0800

    Add files via upload

---
+++ maxxdiff.py
@@ -0,0 +1,27 @@
+def new_index(maxx,minn,my_list):
+    my_list.pop(minn)
+    num=min(my_list)
+    minn=my_list.index(num)
+
+    if maxx>minn:
+        return my_list[maxx] - my_list[minn]
+    elif mann<minn and my_list:
+        return new_index(maxx,minn,my_list)
+    else:
+        return "bad luck"
+
+def maxxdiff(my_list):
+    max_num=max(my_list)
+    maxx=my_list.index(max_num)
+
+    min_num=min(my_list)
+    minn=my_list.index(min_num)
+
+    if maxx>minn:
+        print (my_list[maxx]-my_list[minn])
+    else:
+        sol=new_index(maxx,minn,myy_list)
+        print (sol)
+
+my_list=[9,5,6,8,10]
+print (maxxdiff(my_list))
